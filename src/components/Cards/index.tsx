36e1867ceee3b02b37c80950fef3d95115928cbd
Malo Znal <facebookbaga05@gmail.com>
2024-12-07 08:03:12 +0300

    commit

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -4,18 +4,18 @@ import { FC, useEffect, useState } from "react";
 import { Bean } from "../../types/bean";
 
 type Props = {
-  filterValue: string
-}
+  filterValue: string;
+};
 
-const Cards: FC<Props> = (filterValue) => {
+const Cards: FC<Props> = ({ filterValue }) => {
   const [initialBeans, setinitialBeans] = useState<null | Bean[]>(null);
-  
+  const [updateBeens, setUpdateBeens] = useState<null | Bean[]>(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const getData = async () => {
     try {
-      setIsError(false)
+      setIsError(false);
       setIsLoading(true);
       const req = await fetch(
         "https://jellybellywikiapi.onrender.com/api/Beans"
@@ -23,10 +23,11 @@ const Cards: FC<Props> = (filterValue) => {
       const data = await req.json();
       setIsLoading(false);
       setinitialBeans(data.items);
+      setUpdateBeens(data.items);
     } catch (error) {
       console.log("ERROR->", error);
-      setIsLoading(false)
-      setIsError(true)
+      setIsLoading(false);
+      setIsError(true);
     }
   };
 
@@ -34,12 +35,24 @@ const Cards: FC<Props> = (filterValue) => {
     getData();
   }, []);
 
+  useEffect(() => {
+    if (filterValue) {
+      const newArray: Bean[] | undefined = initialBeans?.filter((item: Bean) =>
+        item.flavorName.includes(filterValue)
+      );
+      if (newArray) {
+        setUpdateBeens(newArray);
+      }
+      console.log("newArray", newArray);
+    }
+  }, [filterValue, initialBeans]);
+
   return (
     <div className={style.container}>
       {isLoading && <p>...loading</p>}
       {isError && <p>...перезагрузите страницу</p>}
-      {initialBeans &&
-        initialBeans.map((bean) => {
+      {updateBeens &&
+        updateBeens.map((bean) => {
           return <Card data={bean} key={bean.beanId} />;
         })}
     </div>
