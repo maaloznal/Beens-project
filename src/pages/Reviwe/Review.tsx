27ba6b84c65ad2b96a4f294b6cab5bd3ddb85fb3
Malo Znal <facebookbaga05@gmail.com>
2025-01-15 22:52:02 +0300

    добавлены секции и категории для них с помощью useForm

--- src/pages/Reviwe/Review.tsx
+++ src/pages/Reviwe/Review.tsx
@@ -1,16 +1,35 @@
 import s from "./s.module.css";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-const Review = () => {
+type FormType = {
+  user_name: string;
+  user_email: string;
+  category: string;
+};
+
+const Review: React.FC = () => {
+  const { register, handleSubmit } = useForm<FormType>();
+
+  const onSubmit: SubmitHandler<FormType> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className={s.formReview}>
-      <input id="user_name" type="text" name="user_name" />
+    <form className={s.formReview} onSubmit={handleSubmit(onSubmit)}>
+      <input id="user_name" type="text" {...register("user_name")} />
       <label htmlFor="user_name">Имя</label>
 
       <label>
-        <input type="email" name="user_email" /> Email
+        <input type="email" {...register("user_email")} /> Email
       </label>
 
       <input type="submit" />
+
+      <select {...register("category")}>
+        <option value="a">Select a</option>
+        <option value="b">Select b</option>
+        <option value="c">Select c</option>
+      </select>
     </form>
   );
 };
